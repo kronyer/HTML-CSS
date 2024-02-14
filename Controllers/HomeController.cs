d237a43ff847acde6e1d84dd0e792ae33145fc6b
kronyer <pedromila2000@gmail.com>
2024-02-14 20:06:50 -0300

    HTML configurado, faltando retoques de CSS

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTML_CSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+            
+        [Route("teste")]
+        public IActionResult Teste()
+        {
+            return View();
+        }
+    }
+}
